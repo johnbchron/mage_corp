f645141bde250740ee706e35c272829b8a31ba88
John Lewis <johnbchron@outlook.com>
2023-08-17 19:09:09 -0500

    corrections to advanced force material

--- src/materials/mod.rs
+++ src/materials/mod.rs
@@ -11,6 +11,8 @@ pub struct MaterialsPlugin;
 
 impl Plugin for MaterialsPlugin {
   fn build(&self, app: &mut App) {
+    app.add_plugins(bevy_shader_utils::ShaderUtilsPlugin);
+    
     load_internal_asset!(
       app,
       PREPASS_SHADER_HANDLE,
