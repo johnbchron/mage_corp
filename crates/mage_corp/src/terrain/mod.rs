d26a357578c0fdf04ad858e5364d709866df259c
John Lewis <johnbchron@outlook.com>
2023-08-24 15:07:15 -0500

    dump

--- crates/mage_corp/src/terrain/mod.rs
+++ crates/mage_corp/src/terrain/mod.rs
@@ -1,1 +1,12 @@
 mod mesh;
+
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub struct TerrainDetailTarget;
+
+#[derive(Resource)]
+pub struct TerrainCurrentGeneration {
+  pub target_location:  Vec3,
+  pub terrain_entities: Vec<Entity>,
+}
