c817d089994a5c2b4f6922cdf352b76d7a999245
John Lewis <github@jlewis.sh>
2023-09-09 14:24:13 -0500

    shape updates & more tests

--- crates/planiscope/src/lib.rs
+++ crates/planiscope/src/lib.rs
@@ -10,12 +10,20 @@ pub enum Shape {
   FidgetRhai { expr: String },
 }
 
+impl Shape {
+  pub fn new_rhai(expr: &str) -> Self {
+    Self::FidgetRhai {
+      expr: expr.to_string(),
+    }
+  }
+}
+
 impl IntoNode for &Shape {
   fn into_node(self, ctx: &mut Context) -> Result<Node, fidget::Error> {
     match self {
       Shape::FidgetRhai { expr } => {
         let mut engine = Engine::new(Some(ctx.clone()));
-        let (node, context) = engine.eval(expr)?;
+        let (node, context) = engine.eval_no_clear(expr)?;
         *ctx = context;
         Ok(node)
       }
@@ -30,7 +38,7 @@ mod tests {
   use super::*;
 
   #[test]
-  fn it_works() {
+  fn uses_a_context() {
     let shape = Shape::FidgetRhai {
       expr: "x + 1".to_string(),
     };
@@ -58,5 +66,41 @@ mod tests {
     let eval_result = eval_result.unwrap();
 
     assert_eq!(eval_result.0, 1.0);
+  }
+
+  #[test]
+  fn converts_to_node() {
+    let mut ctx = Context::new();
+
+    let x_plus_one = (&Shape::new_rhai("x + 1")).into_node(&mut ctx);
+    assert!(x_plus_one.is_ok());
+    let x_plus_one = x_plus_one.unwrap();
+
+    let eval_result = ctx.eval_xyz(x_plus_one, 2.0, 0.0, 0.0);
+    assert!(eval_result.is_ok());
+    let eval_result = eval_result.unwrap();
+
+    assert_eq!(eval_result, 3.0);
+  }
+
+  #[test]
+  fn does_not_mangle_a_context() {
+    let mut ctx = Context::new();
+
+    let x = ctx.x();
+    let one = ctx.constant(1.0);
+    let x_plus_one = ctx.add(x, one).unwrap();
+    let y = ctx.y();
+    let x_plus_one_times_y = ctx.mul(x_plus_one, y).unwrap();
+    println!("{}", ctx.dot());
+
+    let eval_result = ctx.eval_xyz(x_plus_one_times_y, 2.0, 3.0, 0.0).unwrap();
+    assert_eq!(eval_result, 9.0);
+
+    let _shape_node = (&Shape::new_rhai("x + 1")).into_node(&mut ctx).unwrap();
+    println!("{}", ctx.dot());
+
+    let eval_result = ctx.eval_xyz(x_plus_one_times_y, 2.0, 3.0, 0.0).unwrap();
+    assert_eq!(eval_result, 9.0);
   }
 }
