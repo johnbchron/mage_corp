0a19d73e3775892fd63bcee96109201399096734
John Lewis <github@jlewis.sh>
2023-09-20 09:41:26 -0500

    more skeleton logic for foliage

--- crates/mage_corp/src/foliage/mod.rs
+++ crates/mage_corp/src/foliage/mod.rs
@@ -3,29 +3,54 @@ use planiscope::shape::Shape;
 
 use crate::materials::toon::ToonMaterial;
 
-#[derive(Component)]
+#[derive(Component, Default, Reflect)]
+#[reflect(Component)]
 pub struct Foliage {
   shape:    Shape,
-  material: ToonMaterial,
+  material: Handle<ToonMaterial>,
+  #[reflect(ignore)]
   status:   FoliageGenerationStatus,
 }
 
 pub enum FoliageGenerationStatus {
   NotStarted,
-  InProgress(Task<Mesh>),
+  InProgress { task: Task<Mesh> },
   Completed,
+}
+
+impl Default for FoliageGenerationStatus {
+  fn default() -> Self {
+    Self::NotStarted
+  }
 }
 
 pub struct FoliagePlugin;
 
 impl Plugin for FoliagePlugin {
   fn build(&self, app: &mut App) {
-    app.add_systems(Update, flush_foliage);
+    app
+      .register_type::<Foliage>()
+      .add_systems(Startup, spawn_test_foliage)
+      .add_systems(Update, flush_foliage);
   }
 }
 
-pub fn flush_foliage(
+fn spawn_test_foliage(
   mut commands: Commands,
-  mut foliage_q: Query<&mut Foliage>,
+  mut toon_materials: ResMut<Assets<ToonMaterial>>,
 ) {
+  commands.spawn((
+    SpatialBundle::from_transform(Transform::from_xyz(5.0, 5.0, 5.0)),
+    Foliage {
+      shape:    Shape::new_expr("sqrt(square(x) + square(y) + square(z)) - 2"),
+      material: toon_materials.add(ToonMaterial {
+        color: Color::rgb(1.0, 1.0, 0.0),
+        ..default()
+      }),
+      status:   FoliageGenerationStatus::NotStarted,
+    },
+    Name::new("foliage_test"),
+  ));
 }
+
+fn flush_foliage(mut commands: Commands, mut foliage_q: Query<&mut Foliage>) {}
