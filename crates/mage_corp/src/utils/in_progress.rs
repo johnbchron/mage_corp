ccbdc70ac0786bbcf0c42d4d3dabf4f9a191b964
John Lewis <github@jlewis.sh>
2023-09-20 09:46:47 -0500

    added the in_progress module

---
+++ crates/mage_corp/src/utils/in_progress.rs
@@ -0,0 +1,46 @@
+use bevy::{asset::Asset, prelude::*, tasks::Task};
+use futures_lite::future::{block_on, poll_once};
+
+/// A component for handling a task which returns a component.
+///
+/// When the task is completed, the return value is added as a component to the
+/// entity containing the `InProgressComponent`, and the `InProgressComponent`
+/// is removed.
+#[derive(Component)]
+pub struct InProgressComponent<T: Component>(pub Task<T>);
+
+/// A component for handling a task which returns an asset.
+///
+/// When the task is completed, the return value is added to the corresponding
+/// asset collection, the resulting handle is added as a component to the entity
+/// containing the `InProgressAsset`, and the `InProgressAsset` is
+/// removed.
+#[derive(Component)]
+pub struct InProgressAsset<T: Asset>(pub Task<T>);
+
+/// Flushes the results from `InProgressComponent`.
+pub fn in_progress_component_flusher<T: Component>(
+  mut commands: Commands,
+  mut query: Query<(Entity, &mut InProgressComponent<T>)>,
+) {
+  for (entity, mut in_progress) in query.iter_mut() {
+    if let Some(component) = block_on(poll_once(&mut in_progress.0)) {
+      commands.entity(entity).insert(component);
+      commands.entity(entity).remove::<InProgressComponent<T>>();
+    }
+  }
+}
+
+/// Flushes the results from `InProgressAsset`.
+pub fn in_progress_asset_flusher<T: Asset>(
+  mut commands: Commands,
+  mut query: Query<(Entity, &mut InProgressAsset<T>)>,
+  mut asset_collection: ResMut<Assets<T>>,
+) {
+  for (entity, mut in_progress) in query.iter_mut() {
+    if let Some(asset) = block_on(poll_once(&mut in_progress.0)) {
+      commands.entity(entity).insert(asset_collection.add(asset));
+      commands.entity(entity).remove::<InProgressAsset<T>>();
+    }
+  }
+}
--- crates/mage_corp/src/utils/mod.rs
+++ crates/mage_corp/src/utils/mod.rs
@@ -1,4 +1,5 @@
 pub mod despawn;
+pub mod in_progress;
 pub mod timer_lifetime;
 
 pub fn f32_lerp(lhs: f32, rhs: f32, s: f32) -> f32 {
