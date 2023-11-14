9048af4f065817326c9a9d56bbc4058857bc00e7
John Lewis <github@jlewis.sh>
2023-11-14 00:58:20 -0600

    dump

--- crates/mage_corp/src/main.rs
+++ crates/mage_corp/src/main.rs
@@ -42,15 +42,17 @@ fn main() {
         //   }),
         //   ..default()
         // })
-        // .set(WindowPlugin {
-        //   primary_window: Some(Window {
-        //     present_mode: bevy::window::PresentMode::AutoNoVsync,
-        //     ..default()
-        //   }),
-        //   ..default()
-        // })
+        .set(WindowPlugin {
+          primary_window: Some(Window {
+            present_mode: bevy::window::PresentMode::AutoNoVsync,
+            ..default()
+          }),
+          ..default()
+        })
         .set(ImagePlugin::default_nearest()),
     )
+    // other mage_corp crates
+    .add_plugins(bevy_implicits::ImplicitsPlugin)
     // semantics
     .add_plugins(markers::MarkerPlugin)
     // graphics
--- justfile
+++ justfile
@@ -1,7 +1,10 @@
 
 # run the project
 run:
-	MTL_HUD_ENABLED=1 cargo run
+	MTL_HUD_ENABLED=1 cargo run --package mage_corp
+
+run-release:
+	MTL_HUD_ENABLED=1 cargo run --release --package mage_corp
 	
 # run the pre-commit checklist
 pre-c:
