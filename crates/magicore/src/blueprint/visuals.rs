192aebe7d5527732deb899bef6701dbb8318f367
John Lewis <github@jlewis.sh>
2023-12-14 13:50:54 -0600

    added old magic code and deps

---
+++ crates/magicore/src/blueprint/visuals.rs
@@ -0,0 +1,117 @@
+use std::f32::consts::PI;
+
+use bevy::{pbr::NotShadowCaster, prelude::*};
+
+use super::*;
+use crate::materials::force::ForceMaterial;
+
+#[derive(Component, Default, Reflect)]
+struct BlueprintVisualsChild;
+
+#[derive(Resource, Reflect)]
+struct BlueprintVisualsPrefabs {
+  mass_barrier_mesh: Handle<Mesh>,
+}
+
+impl FromWorld for BlueprintVisualsPrefabs {
+  fn from_world(world: &mut World) -> Self {
+    Self {
+      mass_barrier_mesh: world
+        .resource::<AssetServer>()
+        .load("models/icosahedron.stl#wireframe"),
+    }
+  }
+}
+
+#[derive(Bundle, Default)]
+struct BlueprintVisualsBundle<M: Material> {
+  spatial:           SpatialBundle,
+  mesh:              Handle<Mesh>,
+  material:          Handle<M>,
+  marker:            BlueprintVisualsChild,
+  not_shadow_caster: NotShadowCaster,
+}
+
+fn maintain_blueprint_visuals(
+  time: Res<Time>,
+  mut commands: Commands,
+  mut force_materials: ResMut<Assets<ForceMaterial>>,
+  prefabs: Res<BlueprintVisualsPrefabs>,
+  bluep_q: Query<
+    (&ActiveBlueprint, Entity, Option<&Children>),
+    With<Transform>,
+  >,
+  visuals_q: Query<Entity, With<BlueprintVisualsChild>>,
+) {
+  for (bluep, entity, children) in &bluep_q {
+    // if the entity already has `BlueprintVisualsChild` children, delete them
+    if let Some(children) = children {
+      children
+        .iter()
+        .filter_map(|c| visuals_q.get(*c).ok())
+        .for_each(|e| {
+          commands.entity(e).despawn_recursive();
+        });
+    }
+
+    // spawn in the new child
+    commands
+      .entity(entity)
+      .with_children(|p| match bluep.stage {
+        BlueprintStage::Initialized { stored }
+        | BlueprintStage::Built { stored } => match bluep.descriptor {
+          BlueprintDescriptor::MassBarrier { radius, .. } => {
+            let material = force_materials.add(ForceMaterial {
+              color: Color::rgb(0.01, 0.45, 0.81),
+              alpha_min: 0.0005,
+              influence: 10.0,
+              ..default()
+            });
+            for i in 0..3 {
+              let t = (stored / bluep.descriptor.static_cost()).clamp(0.0, 1.0);
+              let t = f32::cbrt(t);
+
+              let rot = Quat::from_euler(
+                EulerRot::XYZ,
+                time.elapsed_seconds() * (i as f32) * PI / 20.0,
+                time.elapsed_seconds() * (((i + 1) % 3) as f32) * PI / 20.0
+                  * 2.0,
+                time.elapsed_seconds() * (((i + 2) % 3) as f32) * PI / 20.0
+                  * 3.0,
+              );
+              let rot = Quat::slerp(rot, Quat::IDENTITY, t);
+
+              p.spawn(BlueprintVisualsBundle {
+                spatial: SpatialBundle::from_transform(
+                  Transform::from_rotation(rot)
+                    .with_scale(Vec3::splat((3.0 - i as f32) / 3.0 * radius)),
+                ),
+                mesh: prefabs.mass_barrier_mesh.clone(),
+                material: material.clone(),
+                ..default()
+              });
+            }
+          }
+        },
+        _ => {}
+      });
+  }
+}
+
+pub struct BlueprintVisualsPlugin;
+
+impl Plugin for BlueprintVisualsPlugin {
+  fn build(&self, app: &mut App) {
+    app
+      .register_type::<BlueprintVisualsChild>()
+      .register_type::<BlueprintVisualsPrefabs>()
+      .init_resource::<BlueprintVisualsPrefabs>()
+      .add_systems(
+        Update,
+        (
+          maintain_blueprint_visuals,
+          // animate_blueprint_visuals
+        ),
+      );
+  }
+}
---
+++ crates/magicore/src/source.rs
@@ -0,0 +1,60 @@
+use bevy::prelude::*;
+
+use super::blueprint::ActiveBlueprint;
+
+#[derive(Component, Reflect)]
+pub struct Source {
+  max_chr_capacity: f32,
+  max_chr_flow:     f32,
+  stored_chr:       f32,
+}
+
+impl Default for Source {
+  fn default() -> Self {
+    Self::new(100.0, 5.0)
+  }
+}
+
+impl Source {
+  pub fn new(max_chr_capacity: f32, max_chr_flow: f32) -> Self {
+    Self {
+      max_chr_capacity,
+      max_chr_flow,
+      stored_chr: max_chr_capacity,
+    }
+  }
+
+  #[allow(dead_code)]
+  pub fn max_flow(&self) -> f32 {
+    self.max_chr_flow
+  }
+
+  #[allow(dead_code)]
+  pub fn max_capacity(&self) -> f32 {
+    self.max_chr_capacity
+  }
+
+  /// Expend a given amount of choranum. The amount must be positive, and the
+  /// total expended is returned. A source cannot expend more than it contains.
+  pub fn expend(&mut self, amount: f32) -> f32 {
+    // make sure amount is positive
+    let amount = amount.max(0.0);
+    let starting_amount = self.stored_chr;
+    // only subtract down to zero
+    self.stored_chr = (self.stored_chr - amount).max(0.0);
+    // return the actual amount expended
+    starting_amount - self.stored_chr
+  }
+
+  pub fn expend_to(&mut self, amount: f32, to: &mut ActiveBlueprint) {
+    to.supply(self.expend(amount))
+  }
+}
+
+pub struct SourcePlugin;
+
+impl Plugin for SourcePlugin {
+  fn build(&self, app: &mut App) {
+    app.register_type::<Source>();
+  }
+}
