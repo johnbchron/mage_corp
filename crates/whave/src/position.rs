0fcfd99d0464fff1f13088f76e1a1549e5d31bc0
John Lewis <github@jlewis.sh>
2023-12-18 12:31:20 -0600

    position docs

--- crates/whave/src/position.rs
+++ crates/whave/src/position.rs
@@ -6,10 +6,13 @@ pub struct Position {
 }
 
 impl Position {
+  /// Creates a new position.
   pub fn new(x: u32, y: u32, z: u32) -> Self { Self { x, y, z } }
+  /// Returns the index of the position in a grid with the given extent.
   pub fn index(&self, extent: &Position) -> usize {
     (self.x + self.y * extent.x + self.z * extent.x * extent.y) as usize
   }
+  /// Returns the position at the given index in a grid with the given extent.
   pub fn from_index(index: usize, extent: &Position) -> Self {
     let index = index as u32;
     let x = index % extent.x;
@@ -17,9 +20,11 @@ impl Position {
     let z = index / (extent.x * extent.y);
     Self { x, y, z }
   }
+  /// Returns whether the position is valid within the given extent.
   pub fn valid(&self, extent: &Position) -> bool {
     self.x < extent.x && self.y < extent.y && self.z < extent.z
   }
+  /// Returns the position transformed by the given offset, if it is valid
   pub fn transform(
     self,
     x: i32,
@@ -38,7 +43,12 @@ impl Position {
       None
     }
   }
+  /// Returns the number of positions in the grid, assuming that `self` is a
+  /// grid extent.
   pub fn grid_count(&self) -> usize { (self.x * self.y * self.z) as usize }
+  /// Returns the positions of all neighbors of the position, excluding
+  /// neighbors that are not valid within the given extent. This is the Moore
+  /// neighborhood in 3D.
   pub fn neighbors(&self, extent: &Position) -> Vec<Self> {
     let mut neighbors = Vec::new();
     for x in -1..=1 {
@@ -55,6 +65,9 @@ impl Position {
     }
     neighbors
   }
+  /// Returns the positions of all direct neighbors of the position, excluding
+  /// neighbors that are not valid within the given extent. This is the von
+  /// Neumann neighborhood in 3D.
   pub fn direct_neighbors(&self, extent: &Position) -> Vec<Self> {
     let mut neighbors = Vec::new();
     for x in -1..=1 {
@@ -80,8 +93,11 @@ impl Position {
     let extent = *self;
     (0..self.grid_count()).map(move |index| Self::from_index(index, &extent))
   }
+  /// The x coordinate of the position.
   pub fn x(&self) -> u32 { self.x }
+  /// The y coordinate of the position.
   pub fn y(&self) -> u32 { self.y }
+  /// The z coordinate of the position.
   pub fn z(&self) -> u32 { self.z }
 }
 
