e8d431b0ca6191fbff0996a78a4784e057474cdc
John Lewis <github@jlewis.sh>
2023-12-21 12:07:22 -0600

    some basic stuff

--- crates/framix/src/lib.rs
+++ crates/framix/src/lib.rs
@@ -1,1 +1,25 @@
+use planiscope::shape::Shape;
 
+pub enum Erosion {
+  LikeNew,
+  Worn,
+  Damaged,
+  Ruined,
+}
+
+pub enum Primitives {
+  Plank {
+    erosion:    Erosion,
+    dimensions: glam::Vec3,
+  },
+  Brick {},
+}
+
+impl From<Primitives> for Shape {
+  fn from(primitive: Primitives) -> Self {
+    match primitive {
+      Primitives::Plank {} => todo!(),
+      Primitives::Brick {} => todo!(),
+    }
+  }
+}
